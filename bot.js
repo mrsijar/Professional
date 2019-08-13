ea1aba957caf002b82c76ae026d708bb4cdfcc5d
mrsijar <54047229+mrsijar@users.noreply.github.com>
2019-08-13 04:07:24 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 50
+    credits: 500
 };
 
 let userData = credits[message.author.id];
@@ -226,7 +226,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 250;
+    let amount = 20000000;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
@@ -247,7 +247,7 @@ client.on('message', async message => {
     
     setTimeout(() => {
        coolDown.remove(message.author.id);
-    },86400000);
+    },0);
     }
 });
 
